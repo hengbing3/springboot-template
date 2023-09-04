2428ae746af5cb7ff169072e7af3ce0efb52a206
Christer <hongweibing3@gmail.com>
2023-09-04 21:04:52 +0800

    Initial commit

---
+++ src/test/java/com/christer/springboottemplate/SpringbootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christer.springboottemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
