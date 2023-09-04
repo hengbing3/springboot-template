8b95787248634c1114061c3c39c975088ba1d9a0
Christer <hongweibing3@gmail.com>
2023-09-04 22:11:03 +0800

    数据拷贝工具类

---
+++ src/main/java/com/christer/springboottemplate/utils/BeanCopyUtilCallBack.java
@@ -0,0 +1,19 @@
+package com.christer.springboottemplate.utils;
+
+/**
+ * @author Christer
+ * @version 1.0
+ * @date 2023-09-04 22:10
+ * Description:
+ * 函数式接口，拷贝数据回调接口
+ */
+@FunctionalInterface
+public interface BeanCopyUtilCallBack <S, T> {
+
+    /**
+     * 定义默认回调方法
+     * @param t
+     * @param s
+     */
+    void callBack(S t, T s);
+}
